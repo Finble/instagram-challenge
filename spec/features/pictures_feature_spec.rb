f8bbaecd978b39680009d81d6fb37d0897ece27e
Finble <elainefinn@hotmail.com>
2016-03-05 10:36:58 +0000

    Rails project set up  - DB created  - Picture controller, view and model (+ routes) set up  - First tests re adding a picture (and message if no picture added) passing :

---
+++ spec/features/pictures_feature_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'pictures' do
+  context 'no pictures have been added' do
+    scenario 'should display a prompt to add a picture' do
+      visit '/pictures'
+      expect(page).to have_content 'No pictures yet'
+      expect(page).to have_link 'Add a picture'
+    end
+  end
+
+  context 'pictures have been added' do
+	  before do
+	    Picture.create(name: 'Holiday')
+	  end
+
+	  scenario 'display pictures' do
+	    visit '/pictures'
+	    expect(page).to have_content('Holiday')
+	    expect(page).not_to have_content('No pictures yet')
+	  end
+	end
+end
